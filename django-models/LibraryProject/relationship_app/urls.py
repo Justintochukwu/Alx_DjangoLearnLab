b45c9d3c5203472525849d692aaf19a1302a31e8
Justintochukwu <justintochukwu00@gmail.com>
2025-07-25 20:19:47 +0100

    samples

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
 
-
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import (
     register_view,
     CustomLoginView,
@@ -16,11 +17,11 @@ from .views import (
 )
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
+    path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     
     # Auth routes
-    path('register/', register_view, name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -23,7 +23,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 # Registration view
-def register_view(request):
+def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
