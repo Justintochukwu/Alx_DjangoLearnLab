cd7cdc9b42eb54a7c485007ab2f2fb9894f2b7ef
Justintochukwu <justintochukwu00@gmail.com>
2025-08-10 19:01:34 +0100

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()       # Get all books from the database
     serializer_class = BookSerializer   # Use our serializer to convert to JSON
-
+    permission_classes = [IsAuthenticated]
+    
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]
