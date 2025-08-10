0d78af1f0dfe7e9362751f8c1f5fe04043e000dd
Justintochukwu <justintochukwu00@gmail.com>
2025-08-10 18:20:10 +0100

    api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()       # Get all books from the database
     serializer_class = BookSerializer   # Use our serializer to convert to JSON
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
