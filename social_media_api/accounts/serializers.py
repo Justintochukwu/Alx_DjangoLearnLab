0b58387bc0e6a7e297acc4f9f183cfa0aa8c4f76
Justintochukwu <justintochukwu00@gmail.com>
2025-08-24 19:23:59 +0100

    media

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,23 +2,21 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ["id", "username", "email", "password"]
 
     def create(self, validated_data):
-        # Create the user properly using create_user()
-        user = User.objects.create_user(
+        # Explicitly use get_user_model().objects.create_user
+        user = get_user_model().objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
         )
-        # Create a token for the new user
         Token.objects.create(user=user)
         return user
 
@@ -27,7 +25,6 @@ class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
-    # We won’t handle login fully here, just validate presence.
     def validate(self, data):
         if not data.get("username") or not data.get("password"):
             raise serializers.ValidationError("Username and password are required")
