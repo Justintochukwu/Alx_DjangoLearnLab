16384f0ed13b50aaee0ce4470202bc0af28ebb33
Justintochukwu <justintochukwu00@gmail.com>
2025-08-25 15:57:10 +0100

    notifications

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import PostCreateView, PostListView, FeedView, index
+from .views import LikePostView, UnlikePostView
 
 urlpatterns = [
     path('', index, name='index'),
     path('create/', PostCreateView.as_view(), name='create-post'),
     path('all/', PostListView.as_view(), name='all-posts'),
     path('feed/', FeedView.as_view(), name='feed'), 
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
