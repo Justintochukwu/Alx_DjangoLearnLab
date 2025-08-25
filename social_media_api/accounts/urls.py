e998b62293bdf2c3ebd813f1501ef86f47c75b2e
Justintochukwu <justintochukwu00@gmail.com>
2025-08-25 14:14:53 +0100

    follows

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', follow_user, name='follow-user'),
+    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user')
 ]
