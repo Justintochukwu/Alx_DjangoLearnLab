8ffb6887656be6cee20a27b3c79efc99a465d732
Justintochukwu <justintochukwu00@gmail.com>
2025-08-26 20:58:37 +0100

    feed

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('feed/', FeedView.as_view(), name='feed'), 
     path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
     path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
-    path('feed/', FeedView.as_view(), name='feed'),
 ]
